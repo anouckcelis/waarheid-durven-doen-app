7c2ac858790cf47f6a7826c913e03e0d1dad1dcf
Anouck Celis <anouck.celis@gmail.com>
2024-05-29 14:21:16 +0200

    aanpassingen voor decumentatie

--- src/Start.js
+++ src/Start.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Algemeen.css';
-import logo from './images/logospotgroup.png';
+import React from 'react';  // Importeer de React module vanuit 'react'
+import { Link } from 'react-router-dom';  // Importeer de Link component vanuit 'react-router-dom'
+import './Algemeen.css';  // Importeer een CSS-bestand genaamd 'Algemeen.css'
+import logo from './images/logospotgroup.png';  // Importeer een afbeelding genaamd 'logospotgroup.png' uit de 'images' map
 
+// Definieer een functionele component genaamd Start
 const Start = () => {
-    return (
-        <div className="container">
-          <div className="start-screen">
-            <div className="intro">
-              <h1>Waarheid<br/>Durven<br/>Doen</h1>
-              <div className="container-start-button">
-                  {/* Link naar de vragen */}
+    return (  // Retourneer een JSX-element
+        <div className="container">  {/* Een div met de class naam 'container' */}
+          <div className="start-screen">  {/* Een div met de class naam 'start-screen' */}
+            <div className="intro">  {/* Een div met de class naam 'intro' */}
+              <h1>Waarheid<br/>Durven<br/>Doen</h1>  {/* Een kop (h1) met de tekst 'Waarheid Durven Doen' */}
+              <div className="container-start-button">  {/* Een div met de class naam 'container-start-button' */}
+                  {/* Een Link component die linkt naar '/WaarheidDurvenDoen' */}
                   <Link to="/WaarheidDurvenDoen">
-                      <button className="start-button">Start Spel</button>
+                      <button className="start-button">Start Spel</button>  {/* Een knop met de class naam 'start-button' en de tekst 'Start Spel' */}
                   </Link>
               </div>
-              <div className="logo">
+              <div className="logo">  {/* Een div met de class naam 'logo' */}
+                      {/* Een link naar 'https://spotgroup.be/' met daarin een afbeelding 'logo' */}
                       <a href="https://spotgroup.be/"><img src={logo} alt="logo SpotGroup"/></a>  
               </div> 
             </div>
@@ -24,4 +26,4 @@ const Start = () => {
     );
 }
 
-export default Start;
+export default Start;  // Exporteer de Start component als de standaard export
